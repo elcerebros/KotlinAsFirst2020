c19cd17d351bae7a8b3fbf989feb0fe6a9eb2dc9
elcerebros <70847355+elcerebros@users.noreply.github.com>
2020-11-10 14:33:45 +0300

    Исправлены замечания в lesson4

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -310,7 +310,7 @@ fun decimalFromString(str: String, base: Int): Int = TODO()
  */
 fun roman(n: Int): String {
     var result = buildString { }
-    val alphabet = listOf<Pair<Int, String>>(Pair(1000, "M"), Pair(900, "CM"), Pair(500, "D"),
+    val alphabet = listOf(Pair(1000, "M"), Pair(900, "CM"), Pair(500, "D"),
             Pair(400, "CD"), Pair(100, "C"), Pair(90, "XC"), Pair(50, "L"), Pair(40, "XL"),
             Pair(10, "X"), Pair(9, "IX"), Pair(5, "V"), Pair(5, "V"), Pair(4, "IV"), Pair(1, "I"))
     var x = n
